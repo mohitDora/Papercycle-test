cad640a68582f3090b29304f4c1cf2ef7524ee21
mohitDora <doramohitkumar@gmail.com>
2024-06-20 11:08:48 +0530

    ch-16

--- src/Components/Shared/Layout.jsx
+++ src/Components/Shared/Layout.jsx
@@ -1,39 +1,39 @@
 "use client";
 
 import React from "react";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import { StoreProvider, useStoreContext } from "../../../lib/Context/store";
 import theme from "../../../lib/Theme/theme";
 import Sonner from "./Sonner";
 import { StyledEngineProvider } from "@mui/material";
 
 const LayoutContent = ({ children }) => {
-    const { snackbarOpen, handleSnackbarClose, sonner } = useStoreContext();
+  const { snackbarOpen, handleSnackbarClose, sonner } = useStoreContext();
 
-    return (
-        <>
-            <Sonner
-                open={snackbarOpen}
-                handleClose={handleSnackbarClose}
-                severity={sonner.severity}
-                message={sonner.message}
-            />
-            {children}
-        </>
-    );
+  return (
+    <>
+      <Sonner
+        open={snackbarOpen}
+        handleClose={handleSnackbarClose}
+        severity={sonner.severity}
+        message={sonner.message}
+      />
+      {children}
+    </>
+  );
 };
 
 const Layout = ({ children }) => {
-    return (
-        <StoreProvider>
-            <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={theme}>
-                <CssBaseline></CssBaseline>
-                <LayoutContent>{children}</LayoutContent>
-            </ThemeProvider>
-            </StyledEngineProvider>
-        </StoreProvider>
-    );
+  return (
+    <StoreProvider>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          {/* <CssBaseline></CssBaseline> */}
+          <LayoutContent>{children}</LayoutContent>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </StoreProvider>
+  );
 };
 
 export default Layout;
